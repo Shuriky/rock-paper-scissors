b6a1689b7a00d8656cd1f6b94e3f17fb2b771389
Shuriky <hdoo0027@student.monash.edu>
2025-03-17 05:13:03 +1100

    add bot's logic

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Hello World")
+
+// Generate a random number between 0 and max
+function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
+
+function getComputerChoice() {
+    let x = getRandomInt(2);
+    let result = "";
+    if (x == 0) {
+        result = "rock";
+    }
+    else if (x == 1) {
+        result = "paper";
+    }
+    else if (x == 2) {
+        result = "scissors";
+    }
+    return result
+}
+
